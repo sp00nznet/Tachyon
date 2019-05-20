1277622ad16dcb0c0f8b66dd33ab43d7ba13c7bd
Campbell Suter <znix@znix.xyz>
2019-05-20 21:22:04 +1200

    Add Direction.fromPoint helper

--- src/main/java/xyz/znix/xftl/math/Direction.kt
+++ src/main/java/xyz/znix/xftl/math/Direction.kt
@@ -9,4 +9,18 @@ enum class Direction(override val x: Int, override val y: Int) : IPoint {
     DOWN_LEFT(-1, 1),
     LEFT(-1, 0),
     LEFT_UP(-1, -1);
+
+    companion object {
+        fun fromPoint(point: IPoint): Direction? {
+            if (point is Direction)
+                return point
+
+            for (p in values()) {
+                if (p posEq point)
+                    return p
+            }
+
+            return null
+        }
+    }
 }
