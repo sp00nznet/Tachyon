e35e1c4ac36878d5a88db74061574abcde6e74b5
Campbell Suter <znix@znix.xyz>
2019-05-25 00:27:01 +1200

    Redo the weapon rendering system using the OpenGL view matrix

--- src/main/java/xyz/znix/xftl/systems/Weapons.kt
+++ src/main/java/xyz/znix/xftl/systems/Weapons.kt
@@ -4,6 +4,7 @@ import org.jdom2.Element
 import org.newdawn.slick.Graphics
 import org.newdawn.slick.Image
 import xyz.znix.xftl.AbstractSystem
+import xyz.znix.xftl.math.ConstPoint
 import xyz.znix.xftl.math.Point
 
 class Weapons(elem: Element) : AbstractSystem("weapons", elem) {
@@ -12,8 +13,7 @@ class Weapons(elem: Element) : AbstractSystem("weapons", elem) {
             hp.weapon?.update(dt)
     }
 
-    // override fun drawBackground(g: Graphics) {
-    override fun drawForeground(g: Graphics) {
+    override fun drawBackground(g: Graphics) {
         for (hp in ship.hardpoints) {
             val blueprint = hp.weapon ?: continue
 
@@ -26,6 +26,8 @@ class Weapons(elem: Element) : AbstractSystem("weapons", elem) {
                 projectile.start().draw(animPnt.x.toFloat(), animPnt.y.toFloat())
             }
 
+            g.pushTransform()
+
             run {
                 val anim = weaponAnimations.getValue(blueprint.type.launcher)
                 val start = anim.start()
@@ -33,37 +35,41 @@ class Weapons(elem: Element) : AbstractSystem("weapons", elem) {
                 start.setCurrentFrame((anim.chargedFrame * blueprint.chargeProgress).toInt())
 
                 var launcher: Image = start.currentFrame
-                val pnt = Point(hp.x, hp.y)
+
+                // We start in ship space - x and y are relative to the hull
+
+                g.translate(hp.x.toFloat(), hp.y.toFloat())
+
+                // We are now in hardpoint-xy space - x and y are relative to the hardpoint's xy,
+                // but rotation is independant
+
+                if (hp.rotate)
+                    g.rotate(0f, 0f, 90f)
+
+                // We are now properly in hardpoint space, y+ is always forward and y- is always backwards
 
                 if (hp.mirror) {
-                    val margin = launcher.height - anim.mountPoint.y
-                    launcher = launcher.getFlippedCopy(true, false)
-                    pnt.y -= margin
+                    g.scale(-1f, 1f)
                 }
 
-                pnt -= anim.mountPoint
+                // We are now in image space - any given XY value here will line up to the pixel
+                // with the same XY on the launcher image.
 
-                launcher.setCenterOfRotation(anim.mountPoint.x.toFloat(), anim.mountPoint.y.toFloat())
-
-                // launcher.rotation = ((System.currentTimeMillis() / 4) % 360).toFloat()
-                launcher.rotation = if (hp.rotate) 90f else 0f
+                g.translate(-anim.mountPoint.x.toFloat(), -anim.mountPoint.y.toFloat())
 
                 // TODO how much are the weapons retracted by?
 
-                launcher.draw(pnt.x.toFloat(), pnt.y.toFloat(), launcher.width.toFloat(), launcher.height.toFloat())
+                launcher.draw(0f, 0f)
 
                 // Draw the charging glow, if present
                 if (blueprint.isCharged) return@run
-                var glow = anim.chargeImage ?: return@run
+                val glow = anim.chargeImage ?: return@run
 
-                if (hp.mirror)
-                    glow = glow.getFlippedCopy(true, false)
-
-                glow.setCenterOfRotation(launcher.centerOfRotationX, launcher.centerOfRotationY)
-                glow.rotation = launcher.rotation
                 glow.alpha = blueprint.chargeProgress
-                glow.draw(pnt.x.toFloat(), pnt.y.toFloat())
+                glow.draw(0f, 0f)
             }
+
+            g.popTransform()
         }
     }
 }
