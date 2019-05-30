88793912fdf98fd276ee626f6b8486180dce612f
Campbell Suter <znix@znix.xyz>
2019-05-30 17:25:44 +1200

    Correctly mark Shields as a main system

--- src/main/java/xyz/znix/xftl/systems/Shields.kt
+++ src/main/java/xyz/znix/xftl/systems/Shields.kt
@@ -3,5 +3,5 @@ package xyz.znix.xftl.systems
 import org.jdom2.Element
 import xyz.znix.xftl.AbstractSystem
 
-class Shields(elem: Element) : AbstractSystem("shields", elem) {
+class Shields(elem: Element) : MainSystem("shields", elem) {
 }
