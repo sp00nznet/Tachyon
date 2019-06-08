edafaef58f935d6f7d0ad01d722898ef8827fa22
Campbell Suter <znix@znix.xyz>
2019-06-08 20:32:25 +1200

    Implent the proper power systems for the shields

--- src/main/java/xyz/znix/xftl/systems/Shields.kt
+++ src/main/java/xyz/znix/xftl/systems/Shields.kt
@@ -1,8 +1,28 @@
 package xyz.znix.xftl.systems
 
 import org.jdom2.Element
-import xyz.znix.xftl.AbstractSystem
 
 class Shields(elem: Element) : MainSystem("shields", elem) {
     override val sortingType: SortingType get() = SortingType.SHIELD
+
+    var selectedShieldBars: Int = 0
+
+    override val selectedEnergyLevel: Int get() = selectedShieldBars * 2
+
+    override fun powerStateChanged() {
+        if (powerAvailable < selectedEnergyLevel)
+            selectedShieldBars = Math.floorDiv(powerAvailable, 2)
+    }
+
+    override fun increasePower() {
+        selectedShieldBars++
+        powerStateChanged()
+    }
+
+    override fun decreasePower() {
+        if (selectedShieldBars > 0)
+            selectedShieldBars--
+
+        powerStateChanged()
+    }
 }
