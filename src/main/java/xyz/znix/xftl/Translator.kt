518fd820ec6e1831bd5104832c5df3216ba75570
Campbell Suter <znix@znix.xyz>
2020-02-04 16:14:39 +1300

    Correctly load the English translation, since it's spread across multiple files

--- src/main/java/xyz/znix/xftl/Translator.kt
+++ src/main/java/xyz/znix/xftl/Translator.kt
@@ -4,18 +4,30 @@ class Translator(df: Datafile, lang: String) {
     val translations: Map<String, String>
 
     init {
-        val filename = when (lang) {
-            "en" -> "data/text_blueprints.xml"
-            "zh" -> "data/text-zh-Hans.xml"
-            else -> "data/text-$lang.xml"
-        }
-        val doc = df.parseXML(df[filename])
+        translations = HashMap()
+        fun load(name: String) = parseFile(name, df, translations)
 
-        translations = HashMap(doc.rootElement.children.size)
+        when (lang) {
+            "en" -> {
+                load("data/text_achievements.xml")
+                load("data/text_blueprints.xml")
+                load("data/text_events.xml")
+                load("data/text_misc.xml")
+                load("data/text_sectorname.xml")
+                load("data/text_tooltips.xml")
+                load("data/text_tutorial.xml")
+            }
+            "zh" -> load("data/text-zh-Hans.xml")
+            else -> load("data/text-$lang.xml")
+        }
+    }
+
+    private fun parseFile(filename: String, df: Datafile, tl: HashMap<String, String>) {
+        val doc = df.parseXML(df[filename])
 
         for (elem in doc.rootElement.children) {
             check(elem.name == "text")
-            translations[elem.getAttributeValue("name")] = elem.textTrim
+            tl[elem.getAttributeValue("name")] = elem.textTrim
         }
     }
 
