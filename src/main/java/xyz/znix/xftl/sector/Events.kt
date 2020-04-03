c6d4dc75684cb0f5e982958718f496e535dff966
Campbell Suter <znix@znix.xyz>
2020-04-03 20:07:36 +1300

    Load item_modify tags on events

--- src/main/java/xyz/znix/xftl/sector/Events.kt
+++ src/main/java/xyz/znix/xftl/sector/Events.kt
@@ -1,6 +1,8 @@
 package xyz.znix.xftl.sector
 
 import org.jdom2.Element
+import org.newdawn.slick.Image
+import xyz.znix.xftl.game.SlickGame
 
 /**
  * Represents something that can be used as an event. This is either an Event or
@@ -15,6 +17,29 @@ interface IEvent {
 class Event(val text: IEventText?, val choices: List<Choice>, elem: Element, val debugId: String) : IEvent {
     val isDistressBeacon: Boolean = elem.getChild("distressBeacon") != null
     val isStore: Boolean = elem.getChild("store") != null
+
+    val itemsModifySteal: Boolean?
+    val itemsModify: Map<Resource, IntRange>
+
+    init {
+        val modElem = elem.getChild("item_modify")
+        if (modElem != null) {
+            itemsModifySteal = modElem.getAttributeValue("steal")?.toBoolean() ?: false
+            val items = HashMap<Resource, IntRange>()
+            itemsModify = items
+
+            for (child in modElem.children) {
+                check(child.name == "item")
+                val type = Resource.byName(child.getAttributeValue("type"))
+                val min = child.getAttributeValue("min").toInt()
+                val max = child.getAttributeValue("max").toInt()
+                items[type] = min..max
+            }
+        } else {
+            itemsModifySteal = null
+            itemsModify = emptyMap()
+        }
+    }
 
     override fun resolve() = this
 }
@@ -40,3 +65,36 @@ class TextList(val name: String, val items: List<IEventText>) : IEventText {
 class EventText(val localised: String) : IEventText {
     override fun resolve(): String = localised
 }
+
+enum class Resource {
+    FUEL,
+    MISSILES,
+    DRONES,
+    SCRAP;
+
+    private var image: Image? = null
+
+    fun getIcon(game: SlickGame): Image {
+        image?.let { return it }
+
+        val img = game.getImg("img/ui_icons/icon_${name.toLowerCase()}.png")
+        image = img
+        return img
+    }
+
+    companion object {
+        fun byName(name: String): Resource = when (name.toLowerCase()) {
+            "fuel" -> FUEL
+            "missiles" -> MISSILES
+            "drones" -> DRONES
+            "scrap" -> SCRAP
+
+            // ... and then 'missile' is used once by ROCK_STARSHIP_MINE
+            "missile" -> MISSILES
+
+            else -> throw IllegalArgumentException("Invalid resource name $name")
+        }
+    }
+}
+
+typealias ResourceSet = Map<Resource, Int>
