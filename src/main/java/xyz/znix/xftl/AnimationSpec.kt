ef3c29bd42d812ced11485d4432dca74cb6d491d
Campbell Suter <znix@znix.xyz>
2020-04-13 20:52:45 +1200

    Add methods to play an AnimationSpec backwards or at a different speed

--- src/main/java/xyz/znix/xftl/AnimationSpec.kt
+++ src/main/java/xyz/znix/xftl/AnimationSpec.kt
@@ -3,12 +3,42 @@ package xyz.znix.xftl
 import org.newdawn.slick.Animation
 import org.newdawn.slick.Image
 import org.newdawn.slick.SpriteSheet
+import java.lang.reflect.Field
 
 class AnimationSpec(val sheet: SpriteSheet, val x: Int, val y: Int, val length: Int, val time: Float) {
-    fun start() = Animation(sheet, x, y, x + length - 1, y, true, (1000 * time).toInt(), false)
+    fun start() = start(1f, false)
+
+    fun start(tmult: Float, backwards: Boolean): Animation {
+        val anim = Animation(sheet, x, y, x + length - 1, y, true, (tmult * 1000 * time).toInt(), false)
+
+        // Horrid solution since this is inaccessible, but what else can we do?
+        if (backwards) {
+            val frames = ANIMATION_FRAMES.get(anim) as ArrayList<*>
+            frames.reverse()
+        }
+
+        return anim
+    }
+
+    /**
+     * It would appear some weapon animations run half as fast as what's set in their XML. No idea why, and
+     * it doesn't happen on crew animations (or at least rock putting out fire, which was the one I tested).
+     */
+    fun startHalfSpeed() = start(2f, false)
 
     fun spriteAt(i: Int): Image {
         if (i >= length) throw IndexOutOfBoundsException(i)
         return sheet.getSprite(x + i, y)
+    }
+
+    companion object {
+        @JvmStatic
+        private val ANIMATION_FRAMES: Field
+
+        init {
+            val af = Animation::class.java.getDeclaredField("frames")
+            af.isAccessible = true
+            ANIMATION_FRAMES = af
+        }
     }
 }
