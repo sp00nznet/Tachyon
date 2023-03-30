6203b5b7512b9a066856e292e55ee804295339ca
Campbell Suter <znix@znix.xyz>
2023-03-30 23:16:43 +1300

    Add the 'weapon' console command
    This fuzzy-searches the available weapons, and shows them in a line
    under the console using the same card-and-icon BlueprintButton as a
    store.

--- src/main/java/xyz/znix/xftl/devutil/DebugConsole.kt
+++ src/main/java/xyz/znix/xftl/devutil/DebugConsole.kt
@@ -4,9 +4,15 @@ import org.newdawn.slick.Color
 import org.newdawn.slick.GameContainer
 import org.newdawn.slick.Graphics
 import org.newdawn.slick.Input
+import xyz.znix.xftl.Blueprint
 import xyz.znix.xftl.Ship
 import xyz.znix.xftl.f
+import xyz.znix.xftl.game.ButtonImageSet
+import xyz.znix.xftl.game.Buttons
 import xyz.znix.xftl.game.SlickGame
+import xyz.znix.xftl.math.ConstPoint
+import xyz.znix.xftl.weapons.ShipWeaponBlueprint
+import java.util.*
 
 /**
  * A development console, to quickly do stuff like load events or get scrap.
@@ -15,6 +21,7 @@ class DebugConsole(val game: SlickGame, val ship: Ship) {
     private val history = ArrayList<String>()
     private var historyCursor: Int = -1
 
+    private var continued: ContinuedCommand? = null
     private var input: String = ""
 
     private val lines = ArrayList<String>()
@@ -25,8 +32,15 @@ class DebugConsole(val game: SlickGame, val ship: Ship) {
 
     private val commands: List<Cmd> = listOf(
         Cmd("help", 0, this::cmdHelp, "Show the available commands"),
-        Cmd("rich", 0, this::cmdRich, "Get a huge amount of scrap, fuel, drones, and missiles")
+        Cmd("rich", 0, this::cmdRich, "Get a huge amount of scrap, fuel, drones, and missiles"),
+        Cmd("weapon", 0, this::cmdWeapon, "Select a weapon, and add it to the ship's cargo area")
     )
+
+    private val prompt: String get() = continued?.prompt ?: PROMPT
+
+    private val currentLine: String
+        get() = if (historyCursor == -1) input
+        else history[historyCursor]
 
     fun render(gc: GameContainer, g: Graphics) {
         val height = gc.height / 2f
@@ -39,11 +53,7 @@ class DebugConsole(val game: SlickGame, val ship: Ship) {
         val fontHeight = 7
         val lineSpacing = fontHeight + 5 // Some letters are a bit outside the font height
 
-        var inputLine = input
-        if (historyCursor != -1) {
-            inputLine = history[historyCursor]
-        }
-        inputLine = PROMPT + inputLine
+        var inputLine = prompt + currentLine
         if (flashTimer.rem(FLASH_TIME) > FLASH_TIME / 2) {
             inputLine += "_"
         }
@@ -58,6 +68,8 @@ class DebugConsole(val game: SlickGame, val ship: Ship) {
             if (y < 0)
                 break
         }
+
+        continued?.render(gc, g, height)
     }
 
     fun update(gc: GameContainer, dt: Float) {
@@ -69,7 +81,6 @@ class DebugConsole(val game: SlickGame, val ship: Ship) {
             Input.KEY_ENTER -> {
                 selectHistory()
                 runCommand()
-                input = ""
             }
 
             Input.KEY_UP -> {
@@ -136,7 +147,20 @@ class DebugConsole(val game: SlickGame, val ship: Ship) {
         if (history.lastOrNull() != input && input.isNotBlank())
             history.add(input)
 
-        lines.add(PROMPT + input)
+        lines.add(prompt + input)
+
+        // If we're part way through a command that takes multiple
+        // lines of input, run that.
+        continued?.let { cmd ->
+            continued = null
+            cmd.run(input.trim())
+            input = ""
+            return
+        }
+
+        // Ignore empty commands
+        if (input.isBlank())
+            return
 
         // Build the arguments list, ignoring subsequent spaces
         val args = input.split(' ').filter { it.isNotEmpty() }
@@ -155,6 +179,8 @@ class DebugConsole(val game: SlickGame, val ship: Ship) {
         }
 
         cmd.func(args)
+
+        input = ""
     }
 
     private fun cmdHelp(@Suppress("UNUSED_PARAMETER") args: List<String>) {
@@ -164,7 +190,7 @@ class DebugConsole(val game: SlickGame, val ship: Ship) {
         }
     }
 
-    private fun cmdRich(@Suppress("UNUSED_PARAMETER") strings: List<String>) {
+    private fun cmdRich(@Suppress("UNUSED_PARAMETER") args: List<String>) {
         lines.add("Resources added.")
 
         ship.scrap = 5000
@@ -173,7 +199,135 @@ class DebugConsole(val game: SlickGame, val ship: Ship) {
         ship.dronesCount = 99
     }
 
+    private fun cmdWeapon(@Suppress("UNUSED_PARAMETER") args: List<String>) {
+        getWeapon { weapon ->
+            for ((i, bp) in ship.cargoBlueprints.withIndex()) {
+                if (bp != null)
+                    continue
+
+                ship.cargoBlueprints[i] = weapon
+                lines.add("Added weapon ${weapon.translateTitle(game)} to cargo slot ${i + 1}.")
+                return@getWeapon
+            }
+
+            lines.add("No space in cargo hold, can't add weapon.")
+        }
+    }
+
+    private fun getWeapon(callback: (ShipWeaponBlueprint) -> Unit) {
+        continued = object : ContinuedCommand() {
+            // A little caching for the search
+            var lastInput: String? = null
+            val visibleWeapons = ArrayList<Buttons.BlueprintButton>()
+
+            override val prompt: String get() = "WEAPON> "
+
+            var lastLeftClick = false
+
+            override fun run(line: String) {
+                val weapon = game.blueprintManager.blueprints[line]
+                if (weapon == null) {
+                    lines.add("No such blueprint '$line'")
+                    return
+                }
+                if (weapon !is ShipWeaponBlueprint) {
+                    lines.add("Blueprint '$line' is not a weapon - ${weapon.javaClass.name}")
+                    return
+                }
+                callback(weapon)
+            }
+
+            override fun render(gc: GameContainer, g: Graphics, height: Float) {
+                updateSearch()
+
+                val mouseX = gc.input.mouseX
+                val mouseY = gc.input.mouseY
+
+                val leftDown = gc.input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)
+                val clicking = leftDown && !lastLeftClick
+                lastLeftClick = leftDown
+
+                for ((i, button) in visibleWeapons.withIndex()) {
+                    val x = 10 + (button.image.normal.width + 5) * i
+                    val y = height.toInt() + 10
+
+                    if (y > gc.width)
+                        break
+
+                    button.windowOffset = ConstPoint(x, y)
+                    button.update(mouseX, mouseY)
+                    button.draw(g)
+
+                    if (clicking) {
+                        button.mouseDown(Input.MOUSE_LEFT_BUTTON, mouseX, mouseY)
+                    }
+                }
+            }
+
+            private fun updateSearch() {
+                val line = currentLine
+
+                if (lastInput == line)
+                    return
+                lastInput = line
+
+                // Implement a simple fuzzy search, splitting up words.
+                val inputParts = line.split(" ", "_")
+                    .map { it.toLowerCase(Locale.UK) }
+                    .filter { it.isNotBlank() }
+
+                val names = game.blueprintManager.blueprints.keys.mapNotNull {
+                    val parts = it.toLowerCase(Locale.UK).split("_")
+
+                    var score = 0
+
+                    for (part in parts) {
+                        for (ip in inputParts) {
+                            if (part.contains(ip)) {
+                                // Add a per-word weight
+                                score += 10 + ip.length
+                            }
+                        }
+                    }
+
+                    return@mapNotNull if (score == 0) {
+                        null
+                    } else {
+                        Pair(it, score)
+                    }
+                }.sortedBy { it.second }.map { it.first }
+
+                val images = ButtonImageSet.select2(game, "img/storeUI/store_buy_weapons")
+
+                // Use BlueprintButton's rendering
+                class DummyButton(override val blueprint: Blueprint) :
+                    Buttons.BlueprintButton(ConstPoint.ZERO, game, images) {
+                    override fun click(button: Int) {
+                        historyCursor = -1
+                        input = blueprint.name
+                        runCommand()
+                    }
+                }
+
+                val weapons: List<DummyButton> = names.mapNotNull {
+                    val bp = game.blueprintManager.blueprints[it]
+                    val weapon = bp as? ShipWeaponBlueprint ?: return@mapNotNull null
+                    return@mapNotNull DummyButton(weapon)
+                }
+
+                visibleWeapons.clear()
+                visibleWeapons.addAll(weapons)
+            }
+        }
+    }
+
     private data class Cmd(val name: String, val argCount: Int?, val func: (List<String>) -> Unit, val helpText: String)
+
+    private abstract class ContinuedCommand {
+        abstract val prompt: String
+        open fun render(gc: GameContainer, g: Graphics, height: Float) {}
+        abstract fun run(line: String)
+    }
 
     companion object {
         private const val PROMPT = "> "
