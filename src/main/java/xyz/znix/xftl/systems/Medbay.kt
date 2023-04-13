c81e00038ea65aaea87b6a4c9c35f83172b9fe2b
Campbell Suter <znix@znix.xyz>
2023-04-13 22:18:07 +1200

    Implement the medbay
    Now crew health is implemented, this is very simple.

--- src/main/java/xyz/znix/xftl/systems/Medbay.kt
+++ src/main/java/xyz/znix/xftl/systems/Medbay.kt
@@ -1,7 +1,33 @@
 package xyz.znix.xftl.systems
 
 import org.jdom2.Element
+import xyz.znix.xftl.crew.AbstractCrew
+import kotlin.math.min
 
 class Medbay(blueprint: SystemBlueprint, elem: Element) : MainSystem(blueprint, elem) {
     override val sortingType: SortingType get() = SortingType.MEDBAY
+
+    override fun update(dt: Float) {
+        super.update(dt)
+
+        val healthPerSec = when (powerSelected) {
+            1 -> 6.4f
+            2 -> 9.6f
+            3 -> 19.2f
+            else -> 0f
+        }
+
+        val healing = healthPerSec * dt
+
+        for (crew in ship.friendlyCrew) {
+            if (crew.room != room)
+                continue
+
+            // Don't revive dying crewmembers
+            if (crew.currentAction == AbstractCrew.Action.DYING)
+                continue
+
+            crew.health = min(crew.health + healing, crew.maxHealth)
+        }
+    }
 }
