a2c6f02f96497c87d3b792452585e73756602cad
Campbell Suter <znix@znix.xyz>
2023-10-19 01:01:24 +1300

    mac: Don't use the deprecated shader texture sample function
    It was renamed from texture2D to just texture.

--- src/main/resources/shaders/image_rect_frag.glsl
+++ src/main/resources/shaders/image_rect_frag.glsl
@@ -8,7 +8,7 @@ out vec4 colour;
 uniform sampler2D tex;
 
 void main() {
-    colour = texture2D(tex, varying_uv) * varying_colour;
+    colour = texture(tex, varying_uv) * varying_colour;
 
     // This is required for stencil writing, since alpha testing is gone in OpenGL core.
     if (colour.a == 0) {
