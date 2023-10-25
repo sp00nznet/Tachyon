d1a193df3b33ebd143fa4edf754d180f6e63dc66
Campbell Suter <znix@znix.xyz>
2023-10-25 21:15:14 +1300

    win: Detect and grab the ftl.dat path from FTL instances
    This searches through the open windows for FTL, and finds the path of
    ftl.dat from it.
    
    This is useful for things like the Humble Bundle version of the game,
    which aren't installed in a given location. It's easier to tell someone
    to open vanilla up than to paste in the path, because experience suggets
    there's a very good chance they'll make a mistake.

--- src/main/java/xyz/znix/xftl/sys/PlatformSpecific.kt
+++ src/main/java/xyz/znix/xftl/sys/PlatformSpecific.kt
@@ -1,11 +1,11 @@
 package xyz.znix.xftl.sys
 
-import org.lwjgl.system.MemoryStack
-import org.lwjgl.system.MemoryUtil
-import org.lwjgl.system.Platform
-import org.lwjgl.system.Pointer
+import org.lwjgl.PointerBuffer
+import org.lwjgl.system.*
 import org.lwjgl.system.libffi.FFICIF
+import org.lwjgl.system.libffi.FFIType
 import org.lwjgl.system.libffi.LibFFI
+import org.lwjgl.system.windows.WinBase
 import org.lwjgl.system.windows.WindowsLibrary
 import xyz.znix.xftl.game.SaveProfile
 import java.nio.file.Files
@@ -43,8 +43,34 @@ sealed interface PlatformSpecific {
     }
 }
 
+@Suppress("PrivatePropertyName") // Windows function names
 private class WindowsPlatform : PlatformSpecific {
     private val shell32 = WindowsLibrary("Shell32.dll")
+    private val user32 = WindowsLibrary("User32.dll")
+    private val kernel32 = WindowsLibrary("Kernel32.dll")
+    private val psapi = WindowsLibrary("Psapi.dll")
+
+    private val EnumWindows: Long
+    private val EnumWindowsCIF: FFICIF
+    private val WNDENUMPROC: FFICIF
+    private val EnumWindowsProc: CallbackI
+
+    private val GetClassNameA: Long
+    private val GetClassNameACIF: FFICIF
+
+    private val GetWindowThreadProcessId: Long
+    private val GetWindowThreadProcessIdCIF: FFICIF
+
+    private val OpenProcess: Long
+    private val OpenProcessCIF: FFICIF
+
+    private val CloseHandle: Long
+    private val CloseHandleCIF: FFICIF
+
+    private val QueryFullProcessImageNameA: Long
+    private val QueryFullProcessImageNameACIF: FFICIF
+
+    private lateinit var windowEnumTarget: (Int) -> Unit
 
     /**
      * The value of the Windows FOLDERID_SavedGames known folder.
@@ -59,6 +85,11 @@ private class WindowsPlatform : PlatformSpecific {
         // Unfortunately, LWJGL doesn't bind this so we have to go through FFI.
         val fnGetKnownFolderPath = shell32.getFunctionAddress("SHGetKnownFolderPath")
 
+        val abi = when {
+            Pointer.BITS32 -> LibFFI.FFI_STDCALL
+            else -> LibFFI.FFI_DEFAULT_ABI
+        }
+
         MemoryStack.stackPush().use { mem ->
             // Set up the call
             val cif = FFICIF(mem.malloc(FFICIF.SIZEOF))
@@ -69,11 +100,6 @@ private class WindowsPlatform : PlatformSpecific {
                 LibFFI.ffi_type_pointer, // hToken
                 LibFFI.ffi_type_pointer, // ppszPath
             )
-
-            val abi = when {
-                Pointer.BITS32 -> LibFFI.FFI_STDCALL
-                else -> LibFFI.FFI_DEFAULT_ABI
-            }
 
             val prepResult = LibFFI.ffi_prep_cif(
                 cif,
@@ -131,11 +157,256 @@ private class WindowsPlatform : PlatformSpecific {
         }
 
         saveGamePath = userSavedGames.resolve("Project Wormhole")
+
+        // Set up the stuff we need for instance scanning
+        EnumWindows = user32.getFunctionAddress("EnumWindows")
+
+        EnumWindowsCIF = allocateCIF(
+            abi,
+            LibFFI.ffi_type_uint8, // Return bool
+
+            LibFFI.ffi_type_pointer, // WNDENUMPROC (callback)
+            LibFFI.ffi_type_pointer, // LPARAM (user data)
+        )
+
+        WNDENUMPROC = allocateCIF(
+            abi,
+            LibFFI.ffi_type_uint8, // Return bool
+
+            LibFFI.ffi_type_uint32, // HWND (window in question)
+            LibFFI.ffi_type_pointer, // LPARAM (user data)
+        )
+
+        // Allocate a callback for EnumWindows. Do this once even though
+        // it means we can't be thread-safe, since each time we do this
+        // it allocates a little bit of memory.
+        // (that might be meaningful, since it has to be VirtualProtect-ed).
+        EnumWindowsProc = object : CallbackI {
+            override fun getCallInterface(): FFICIF {
+                return WNDENUMPROC
+            }
+
+            override fun callback(ret: Long, args: Long) {
+                val windowHandle = MemoryUtil.memGetInt(MemoryUtil.memGetAddress(args))
+
+                // Ignore the userdata arg.
+
+                // Thread-unsafely run the actual callback
+                windowEnumTarget(windowHandle)
+
+                // Return true to continue enumeration
+                MemoryUtil.memPutByte(ret, 1)
+            }
+        }
+
+        GetClassNameA = user32.getFunctionAddress("GetClassNameA")
+        GetClassNameACIF = allocateCIF(
+            abi,
+            LibFFI.ffi_type_uint32, // return int
+
+            LibFFI.ffi_type_uint32, // HWND
+            LibFFI.ffi_type_pointer, // LPSTR
+            LibFFI.ffi_type_uint32, // int nMaxCount
+        )
+
+        GetWindowThreadProcessId = user32.getFunctionAddress("GetWindowThreadProcessId")
+        GetWindowThreadProcessIdCIF = allocateCIF(
+            abi,
+            LibFFI.ffi_type_uint32, // return DWORD
+
+            LibFFI.ffi_type_uint32, // HWND
+            LibFFI.ffi_type_pointer, // LPDWORD out process id
+        )
+
+        OpenProcess = kernel32.getFunctionAddress("OpenProcess")
+        OpenProcessCIF = allocateCIF(
+            abi,
+            LibFFI.ffi_type_uint32, // return HANDLE
+
+            LibFFI.ffi_type_uint32, // DWORD dwDesiredAccess
+            LibFFI.ffi_type_uint8, // bool bInheritHandle
+            LibFFI.ffi_type_uint32, // DWORD dwProcessId
+        )
+
+        CloseHandle = kernel32.getFunctionAddress("CloseHandle")
+        CloseHandleCIF = allocateCIF(
+            abi,
+            LibFFI.ffi_type_uint8, // return bool
+
+            LibFFI.ffi_type_uint32, // HANDLE
+        )
+
+        QueryFullProcessImageNameA = kernel32.getFunctionAddress("QueryFullProcessImageNameA")
+        QueryFullProcessImageNameACIF = allocateCIF(
+            abi,
+            LibFFI.ffi_type_uint8, // return bool
+
+            LibFFI.ffi_type_uint32, // HANDLE
+            LibFFI.ffi_type_uint32, // DWORD
+            LibFFI.ffi_type_pointer, // LPSTR
+            LibFFI.ffi_type_pointer, // PDWORD
+        )
     }
 
     override fun findRunningInstanceDat(): Path? {
-        // TODO implement
-        return null
+        var result: Path? = null
+
+        windowEnumTarget = callback@{ hwnd ->
+            val className = MemoryStack.stackPush().use { mem ->
+                val returnResult = mem.calloc(4)
+
+                val classNameBuf = mem.calloc(64)
+
+                val argsBuf = mem.pointers(
+                    MemoryUtil.memAddress(mem.ints(hwnd)),
+                    mem.pointers(classNameBuf).address(),
+                    MemoryUtil.memAddress(mem.ints(classNameBuf.remaining()))
+                )
+
+                LibFFI.ffi_call(GetClassNameACIF, GetClassNameA, returnResult, argsBuf)
+
+                val classNameBytes = ByteArray(returnResult.getInt(0))
+                classNameBuf.get(classNameBytes)
+                String(classNameBytes, Charsets.UTF_8)
+            }
+
+            // SIL window classes start with 'SILWindowClass.' and then a random number
+            // (this is why we can't use FindWindowA)
+            // Do this filtering to avoid poking around in every process, as
+            // that's just asking for an antivirus false positive.
+            if (!className.startsWith("SILWindowClass."))
+                return@callback
+
+            // Find the process ID
+            val pid = MemoryStack.stackPush().use { mem ->
+                val returnResult = mem.calloc(4)
+                val pid = mem.calloc(4)
+
+                val argsBuf = mem.pointers(
+                    MemoryUtil.memAddress(mem.ints(hwnd)),
+                    mem.pointers(pid).address()
+                )
+
+                LibFFI.ffi_call(GetWindowThreadProcessIdCIF, GetWindowThreadProcessId, returnResult, argsBuf)
+
+                val threadId = returnResult.getInt(0)
+                if (threadId == 0) {
+                    println("Found FTL window, but couldn't find it's process ID: err=${WinBase.getLastError()}")
+                    return@callback
+                }
+
+                pid.getInt(0)
+            }
+
+            // Open a process handle
+            val handle = MemoryStack.stackPush().use { mem ->
+                val returnResult = mem.calloc(4)
+                val desiredAccess = 0x1000 // PROCESS_QUERY_LIMITED_INFORMATION
+
+                val argsBuf = mem.pointers(
+                    mem.ints(desiredAccess),
+                    mem.ints(0),
+                    mem.ints(pid),
+                )
+
+                LibFFI.ffi_call(OpenProcessCIF, OpenProcess, returnResult, argsBuf)
+                returnResult.getInt(0)
+            }
+
+            // Find the image name
+            val pathString: String = MemoryStack.stackPush().use { mem ->
+                val returnResult = mem.calloc(4)
+
+                val pathBuff = mem.calloc(256)
+                val stringLen = mem.ints(pathBuff.remaining())
+
+                val argsBuf = mem.pointers(
+                    MemoryUtil.memAddress(mem.ints(handle)),
+                    MemoryUtil.memAddress(mem.ints(0)),
+                    mem.pointers(pathBuff).address(),
+                    mem.pointers(stringLen).address(), // in/out
+                )
+
+                LibFFI.ffi_call(QueryFullProcessImageNameACIF, QueryFullProcessImageNameA, returnResult, argsBuf)
+
+                if (returnResult.getInt(0) == 0) {
+                    println("Found FTL window, but couldn't read the executable path: err=${WinBase.getLastError()}")
+
+                    // Don't return immediately, we still have to call CloseHandle
+                    return@use ""
+                }
+
+                val bytes = ByteArray(stringLen.get(0))
+                pathBuff.get(bytes)
+                String(bytes, Charsets.UTF_8)
+            }
+
+            // Close the handle now we're done
+            MemoryStack.stackPush().use { mem ->
+                val returnResult = mem.calloc(4)
+                val argsBuf = mem.pointers(mem.ints(handle))
+                LibFFI.ffi_call(CloseHandleCIF, CloseHandle, returnResult, argsBuf)
+            }
+
+            // We used an empty path if QueryFullProcessImageNameA failed,
+            // in which case we already printed an error.
+            if (pathString == "") {
+                return@callback
+            }
+
+            val path = Path.of(pathString)
+            val datPath = path.parent.resolve("ftl.dat")
+
+            if (!Files.isRegularFile(datPath)) {
+                println("Found FTL window of class $className with PID $pid, path: '$pathString'")
+                println(" ... but there was no ftl.dat file at '$datPath'")
+                return@callback
+            }
+
+            result = datPath
+        }
+
+        // Call EnumWindows
+        MemoryStack.stackPush().use { mem ->
+            val returnResult = mem.calloc(4)
+
+            val argsBuf = mem.pointers(
+                mem.pointers(EnumWindowsProc.address()), // callback
+                mem.pointers(0), // userdata, passed to callback
+            )
+
+            LibFFI.ffi_call(EnumWindowsCIF, EnumWindows, returnResult, argsBuf)
+
+            val enumResult = returnResult.get(0).toInt()
+            if (enumResult != 1) {
+                println("Failed to enumerate windows (searching for FTL instance)")
+                return null
+            }
+        }
+
+        return result
+    }
+
+    private fun allocateCIF(abi: Int, returnType: FFIType, vararg args: FFIType): FFICIF {
+        val cif = FFICIF.create()
+
+        // argTypes isn't copied into the CIF, so it can't go on the stack.
+        val argTypesPtr = MemoryUtil.getAllocator().calloc(args.size.toLong(), Pointer.POINTER_SIZE.toLong())
+        val argTypes = PointerBuffer.create(argTypesPtr, args.size)
+
+        for ((i, type) in args.withIndex()) {
+            argTypes.put(i, type)
+        }
+
+        val prepResult = LibFFI.ffi_prep_cif(
+            cif,
+            abi,
+            returnType,
+            argTypes
+        )
+        check(prepResult == LibFFI.FFI_OK)
+
+        return cif
     }
 }
 
