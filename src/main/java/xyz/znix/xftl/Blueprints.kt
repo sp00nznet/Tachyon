sp00nznet/Tachyon

commit 38ed63bd69d053b4f596324d0a8337c8c9dc3cbf

Add the automated testing system
This lets us write automated tests for various things, to catch
regressions more easily.

src/main/java/xyz/znix/xftl/Blueprints.kt
@@ -1,5 +1,6 @@
 package xyz.znix.xftl
 
+import org.jdom2.Document
 import org.jdom2.Element
 import xyz.znix.xftl.augments.AugEngiMedbots
 import xyz.znix.xftl.augments.AugPreigniter
@@ -12,35 +13,16 @@ import xyz.znix.xftl.systems.SystemBlueprint
 import xyz.znix.xftl.weapons.*
 import kotlin.random.Random
 
-class BlueprintManager(df: Datafile, private val enableAE: Boolean) {
+class BlueprintManager {
     val blueprints: Map<String, IBlueprint>
     val itemBlueprints: Map<String, ItemBlueprint>
 
-    init {
+    private constructor(enableAE: Boolean, loader: (BlueprintManager) -> Unit) {
         blueprints = HashMap()
         itemBlueprints = HashMap()
 
-        loadFile(df, "blueprints.xml")
-        loadFile(df, "autoBlueprints.xml")
-        loadFile(df, "bosses.xml")
-        if (enableAE) {
-            loadFile(df, "dlcBlueprints.xml")
-            loadFile(df, "dlcBlueprintsOverwrite.xml")
-            loadFile(df, "dlcPirateBlueprints.xml")
-        }
-
-        // If AE is enabled, rename all the OVERRIDE blueprints to
-        // remove that prefix - thus they'll be used instead of the
-        // original ones.
-        if (enableAE) {
-            // Use toList to duplicate the entries list, since we'll
-            // be mutating the main blueprints map.
-            for ((name, bp) in blueprints.entries.toList()) {
-                if (!name.startsWith(AE_PREFIX))
-                    continue
-                blueprints[name.removePrefix(AE_PREFIX)] = bp
-            }
-        }
+        // Load all the blueprints
+        loader(this)
 
         // Remove the now-redundant override entries - this is probably
         // unnecessary, but it'll ensure we don't end up accidentally
@@ -55,7 +37,31 @@ class BlueprintManager(df: Datafile, private val enableAE: Boolean) {
             if (bp !is BlueprintList) continue
             bp.cleanup()
         }
+
+        // If AE is enabled, rename all the OVERRIDE blueprints to
+        // remove that prefix - thus they'll be used instead of the
+        // original ones.
+        if (enableAE) {
+            // Use toList to duplicate the entries list, since we'll
+            // be mutating the main blueprints map.
+            for ((name, bp) in blueprints.entries.toList()) {
+                if (!name.startsWith(AE_PREFIX))
+                    continue
+                blueprints[name.removePrefix(AE_PREFIX)] = bp
+            }
+        }
     }
+
+    constructor(df: Datafile, enableAE: Boolean) : this(enableAE, {
+        it.loadFile(df, "blueprints.xml")
+        it.loadFile(df, "autoBlueprints.xml")
+        it.loadFile(df, "bosses.xml")
+        if (enableAE) {
+            it.loadFile(df, "dlcBlueprints.xml")
+            it.loadFile(df, "dlcBlueprintsOverwrite.xml")
+            it.loadFile(df, "dlcPirateBlueprints.xml")
+        }
+    })
 
     // Required to bind sounds to the weapons
     fun finishLoading(content: InGameState.GameContent) {
@@ -76,11 +82,16 @@ class BlueprintManager(df: Datafile, private val enableAE: Boolean) {
         return blueprint.real
     }
 
+
     private fun loadFile(df: Datafile, name: String) {
         val file = df["data/$name"]
-        val mutableBlueprints = blueprints as HashMap<String, IBlueprint>
         val parseXML = df.parseXML(file)
-        for (elem in parseXML.rootElement.children) {
+        loadDocument(df, parseXML.rootElement)
+    }
+
+    private fun loadDocument(df: Datafile, rootElement: Element) {
+        val mutableBlueprints = blueprints as HashMap<String, IBlueprint>
+        for (elem in rootElement.children) {
             // The name 'drones' conflicts with the drones system, and it's the only such name
             // conflict. Since we only need them for stores, put them in a separate list.
             if (elem.name == "itemBlueprint") {
@@ -175,6 +186,18 @@ class BlueprintManager(df: Datafile, private val enableAE: Boolean) {
 
     companion object {
         const val AE_PREFIX = "OVERRIDE_"
+
+        /**
+         * Create a version of [BlueprintManager] for use in automated tests,
+         * loading a custom set of blueprint XML files.
+         */
+        fun createForTests(df: Datafile, files: List<Document>): BlueprintManager {
+            return BlueprintManager(true) {
+                for (doc in files) {
+                    it.loadDocument(df, doc.rootElement)
+                }
+            }
+        }
     }
 }
 

src/main/java/xyz/znix/xftl/Animations.kt
@@ -1,30 +1,37 @@
 package xyz.znix.xftl
 
+import org.jdom2.Document
 import org.jdom2.Element
 import xyz.znix.xftl.game.InGameState
 import xyz.znix.xftl.math.ConstPoint
 import xyz.znix.xftl.rendering.Image
 
-class Animations(df: Datafile) {
+class Animations {
     private val sheets: Map<String, SpriteSheetSpec>
     val animations: Map<String, AnimationSpec>
     val weaponAnimations: Map<String, WeaponAnimationSpec>
 
-    init {
+    constructor(specifications: List<Document>) {
         sheets = HashMap()
         animations = HashMap()
         weaponAnimations = HashMap()
 
-        load(df, "data/animations.xml")
-        load(df, "data/dlcAnimations.xml")
+        for (spec in specifications) {
+            load(spec)
+        }
     }
 
-    private fun load(df: Datafile, filePath: String) {
+    constructor(df: Datafile) : this(
+        listOf(
+            df.parseXML(df["data/animations.xml"]),
+            df.parseXML(df["data/dlcAnimations.xml"])
+        )
+    )
+
+    private fun load(doc: Document) {
         sheets as MutableMap
         animations as MutableMap
         weaponAnimations as MutableMap
-
-        val doc = df.parseXML(df[filePath])
 
         // Parse everything
         // Sadly, duplicate sprite sheets exist in vanilla (eg for the ion bomb,

src/main/java/xyz/znix/xftl/Datafile.kt
@@ -1,6 +1,7 @@
 package xyz.znix.xftl
 
 import org.jdom2.Document
+import org.newdawn.slick.ImageBuffer
 import xyz.znix.xftl.game.MainGame
 import xyz.znix.xftl.modding.*
 import xyz.znix.xftl.rendering.Image
@@ -21,12 +22,29 @@ const val ENTRY_SIZE = 20
 /**
  * Represents a vanilla FTL .dat file, without any mods applied.
  *
+ * This is an interface, so tests can run without the vanilla assets.
+ *
  * [Datafile] acts as a layer over the top of this, applying changes from
  * [SlipstreamPatcher] and any other mod sources we might later have.
  */
-class VanillaDatafile(val underlyingFile: File) {
+interface IVanillaDatafile {
+    fun getAllFiles(): List<VanillaDatafile.Entry>
+    fun containsFile(name: String): Boolean
+    fun read(file: VanillaDatafile.Entry): ByteArray
+
+    /**
+     * True if patched files can be stored in the XML cache.
+     *
+     * This is notably false for automated tests.
+     */
+    val xmlCacheSupported: Boolean
+}
+
+class VanillaDatafile(val underlyingFile: File) : IVanillaDatafile {
     private val files: MutableMap<String, Entry> = HashMap()
     private val fi: RandomAccessFile = RandomAccessFile(underlyingFile, "r")
+
+    override val xmlCacheSupported: Boolean get() = true
 
     init {
         // Skip the 'PKG\n' header
@@ -102,16 +120,16 @@ class VanillaDatafile(val underlyingFile: File) {
     operator fun get(name: String): Entry =
         files[name] ?: throw IllegalArgumentException("No such file '$name'")
 
-    fun containsFile(name: String): Boolean = files.containsKey(name)
+    override fun containsFile(name: String): Boolean = files.containsKey(name)
 
-    fun read(file: Entry): ByteArray {
+    override fun read(file: Entry): ByteArray {
         fi.seek(file.offset.toLong())
         val bytes = ByteArray(file.length)
         fi.read(bytes)
         return bytes
     }
 
-    fun getAllFiles(): List<Entry> {
+    override fun getAllFiles(): List<Entry> {
         return files.values.toList()
     }
 
@@ -126,7 +144,7 @@ class VanillaDatafile(val underlyingFile: File) {
     }
 }
 
-class Datafile(val vanilla: VanillaDatafile, slipstreamMods: List<SlipstreamMod>) {
+open class Datafile(val vanilla: IVanillaDatafile, slipstreamMods: List<SlipstreamMod>) {
     // TODO handle closing this, to close the zip files
     private val patcher = SlipstreamPatcher(vanilla)
     private val xmlCache = PatchedXMLCache(PlatformSpecific.INSTANCE.xmlCacheDirectory)
@@ -159,6 +177,7 @@ class Datafile(val vanilla: VanillaDatafile, slipstreamMods: List<SlipstreamMod>
         // files could build up over time from old mods, and because they're not
         // usually patched by other mods and are thus relatively quick to parse.
         val canCache = source !is VanillaFileSource && vanilla.containsFile(file.name)
+                && vanilla.xmlCacheSupported
 
         if (canCache) {
             // Try to re-use a previous copy of this file, if possible.
@@ -180,7 +199,7 @@ class Datafile(val vanilla: VanillaDatafile, slipstreamMods: List<SlipstreamMod>
         return ByteArrayInputStream(read(file))
     }
 
-    fun readImage(context: ResourceContext, file: FTLFile): Image {
+    open fun readImage(context: ResourceContext, file: FTLFile): Image {
         return TextureLoader.loadImage(context, open(file), file.name)
     }
 

src/main/java/xyz/znix/xftl/game/InGameState.java
@@ -41,6 +41,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class InGameState extends MainGame.GameState {
+    @Nullable // Null only when running tests
     private final MainGame mainGame;
 
     private final GameContent content;
@@ -109,8 +110,8 @@ public class InGameState extends MainGame.GameState {
      */
     private final ArrayList<LivingCrew> playerCrew = new ArrayList<>();
 
-    public InGameState(MainGame mainGame, GameContent content, GameContainer container, String playerShipName, Difficulty difficulty, EditableShip customised) {
-        this(mainGame, content, container);
+    public InGameState(MainGame mainGame, GameContent content, String playerShipName, Difficulty difficulty, EditableShip customised) {
+        this(mainGame, content);
         this.difficulty = difficulty;
 
         gameMap = new GameMap(df, eventManager, enableAdvancedEdition, Random.Default);
@@ -125,18 +126,22 @@ public class InGameState extends MainGame.GameState {
         // Do this after setting the initial beacon, since the ship reads the current
         // beacon when calculating its power values.
         createNewPlayerShip(playerShipName, customised);
-        shipUI = new PlayerShipUI(player, this);
+
+        // Don't create PlayerShipUI in automated tests
+        if (mainGame != null) {
+            shipUI = new PlayerShipUI(player, this);
+        }
 
         // Show the starting beacon dialogue, since it wasn't displayed earlier
         // due to shipUI not existing yet.
-        shipUI.showEventDialogue(currentBeacon.getEvent());
+        showEventDialogue(currentBeacon.getEvent());
     }
 
     /**
      * Load a previously-saved game.
      */
-    public InGameState(MainGame mainGame, GameContent content, GameContainer container, Document saveGame) {
-        this(mainGame, content, container);
+    public InGameState(MainGame mainGame, GameContent content, Document saveGame) {
+        this(mainGame, content);
 
         Element root = saveGame.getRootElement();
         if (!root.getName().equals("xftlSaveGame")) {
@@ -175,14 +180,14 @@ public class InGameState extends MainGame.GameState {
         // To do this, just check if it's already playing something. Since
         // the sound manager is part of the game content, it's not cleared
         // across a reload.
-        if (getSounds().getCurrentMusic() == null) {
+        if (getSounds() instanceof RealSoundManager real && real.getCurrentMusic() == null) {
             loadSectorMusic(currentBeacon.getSector().getType());
         }
 
         updatePlayerCrew();
     }
 
-    private InGameState(MainGame mainGame, GameContent content, GameContainer container) {
+    private InGameState(MainGame mainGame, GameContent content) {
         this.mainGame = mainGame;
         this.content = content;
 
@@ -191,6 +196,10 @@ public class InGameState extends MainGame.GameState {
         enableAdvancedEdition = content.enableAdvancedEdition;
         blueprintManager = content.blueprintManager;
         eventManager = content.eventManager;
+
+        // Don't load any assets while running automated tests
+        if (!(df.getVanilla() instanceof VanillaDatafile))
+            return;
 
         pauseFont = getFont("c&c");
 
@@ -318,11 +327,14 @@ public class InGameState extends MainGame.GameState {
 
         hoveredRoom = null;
 
-        content.sounds.updateLoopedSounds(isPaused());
-        content.sounds.setCombatMusic(isInDanger());
-        content.sounds.updateMusic(delta);
-        content.sounds.setSoundEffectVolume(mainGame.getProfile().getSoundVolume());
-        content.sounds.setMusicVolume(mainGame.getProfile().getMusicVolume());
+        // Don't touch sounds in automated tests
+        if (content.sounds instanceof RealSoundManager sounds) {
+            sounds.updateLoopedSounds(isPaused());
+            sounds.setCombatMusic(isInDanger());
+            sounds.updateMusic(delta);
+            sounds.setSoundEffectVolume(mainGame.getProfile().getSoundVolume());
+            sounds.setMusicVolume(mainGame.getProfile().getMusicVolume());
+        }
 
         if (in.isKeyPressed(Input.KEY_GRAVE)) {
             debugConsoleVisible = !debugConsoleVisible;
@@ -571,10 +583,18 @@ public class InGameState extends MainGame.GameState {
         }
     }
 
-    private void updateGameState(float dt) {
+    /**
+     * Update the in-game state.
+     * <p>
+     * This is only public so that automated tests can call it - it should
+     * NOT be called elsewhere from the main code.
+     */
+    public void updateGameState(float dt) {
         currentBeacon.getEnvironment(this).update(dt);
 
-        shipUI.update();
+        if (shipUI != null)
+            shipUI.update();
+
         player.update(dt);
 
         updatePlayerCrew();
@@ -599,7 +619,7 @@ public class InGameState extends MainGame.GameState {
                 if (enemy.getSpec() != null && enemyIsHostile) {
                     IEvent event = enemy.getSpec().getDestroyed();
                     if (event != null)
-                        shipUI.showEventDialogue(event.resolve());
+                        showEventDialogue(event.resolve());
                 }
 
                 if (enemy.isFlagship()) {
@@ -627,7 +647,7 @@ public class InGameState extends MainGame.GameState {
                 currentBeacon.setShip(null);
 
                 if (jumpEvent != null) {
-                    shipUI.showEventDialogue(jumpEvent.resolve());
+                    showEventDialogue(jumpEvent.resolve());
                 }
 
                 return;
@@ -642,11 +662,11 @@ public class InGameState extends MainGame.GameState {
                 if (enemy.getSpec() != null) {
                     IEvent event = enemy.getSpec().getDeadCrew();
                     if (event != null)
-                        shipUI.showEventDialogue(event.resolve());
+                        showEventDialogue(event.resolve());
                 } else if (enemy.isFlagship()) {
                     // TODO turn the enemy into an autoscout
                     // Event event = eventManager.get("BOSS_AUTOMATED").resolve();
-                    // shipUI.showEventDialogue(event);
+                    // showEventDialogue(event);
                 }
 
                 setEnemyIsHostile(false);
@@ -700,7 +720,7 @@ public class InGameState extends MainGame.GameState {
 
             player.resetAfterJump();
             if (currentBeacon.getState() == Beacon.State.UNVISITED) {
-                shipUI.showEventDialogue(currentBeacon.getEvent());
+                showEventDialogue(currentBeacon.getEvent());
             }
         }
 
@@ -727,7 +747,7 @@ public class InGameState extends MainGame.GameState {
             }
 
             Event event = eventManager.get(eventName).resolve();
-            shipUI.showEventDialogue(event);
+            showEventDialogue(event);
         }
 
         // If the flagship is here, spawn it in. Doing this last overwrites
@@ -788,7 +808,7 @@ public class InGameState extends MainGame.GameState {
         setEnemyIsHostile(true);
 
         Event event = eventManager.get("BOSS_TEXT_" + stage).resolve();
-        shipUI.showEventDialogue(event);
+        showEventDialogue(event);
     }
 
     private void onFlagshipKilled() {
@@ -818,7 +838,7 @@ public class InGameState extends MainGame.GameState {
 
             // The event when you defeat a stage
             Event event = eventManager.get("BOSS_ESCAPED").resolve();
-            shipUI.showEventDialogue(event);
+            showEventDialogue(event);
         }
     }
 
@@ -1347,6 +1367,13 @@ public class InGameState extends MainGame.GameState {
      * Think carefully about why you need to use this!
      */
     public MainGame getMainGame() {
+        // This is only used for stuff like switching out of the main state,
+        // which we don't currently support testing. Since very few things
+        // do that, automated testing for that is overkill.
+        if (mainGame == null) {
+            throw new UnsupportedOperationException("Cannot get main state while running tests");
+        }
+
         return mainGame;
     }
 
@@ -1716,6 +1743,13 @@ public class InGameState extends MainGame.GameState {
         return crewCount;
     }
 
+    void showEventDialogue(Event event) {
+        // The UI is null for automated tests
+        if (shipUI != null) {
+            shipUI.showEventDialogue(event);
+        }
+    }
+
     public boolean isPlayerCrewFull() {
         return getPlayerCrewCount() >= 8;
     }
@@ -1775,7 +1809,7 @@ public class InGameState extends MainGame.GameState {
 
             blueprintManager = new BlueprintManager(datafile, enableAdvancedEdition);
             animations = new Animations(datafile);
-            sounds = new SoundManager(datafile, resourceContext);
+            sounds = new RealSoundManager(datafile, resourceContext);
             generator = new ShipGenerator(datafile, blueprintManager);
             translator = new Translator(datafile, "en");
             eventManager = new EventManager(datafile, translator, blueprintManager);
@@ -1793,6 +1827,32 @@ public class InGameState extends MainGame.GameState {
             } catch (JDOMException | IOException e) {
                 throw new RuntimeException("Failed to load ship data", e);
             }
+        }
+
+        /**
+         * Wrap some pre-loaded content.
+         * <p>
+         * This is used by automated tests.
+         */
+        public GameContent(
+                Datafile datafile, boolean enableAdvancedEdition,
+                BlueprintManager blueprintManager, EventManager eventManager,
+                CrewNameManager nameManager, Animations animations,
+                SoundManager sounds, Translator translator,
+                ShipGenerator generator, Achievement.AchievementTable achievements,
+                ShipFamily.FamilyTable shipFamilies
+        ) {
+            this.datafile = datafile;
+            this.enableAdvancedEdition = enableAdvancedEdition;
+            this.blueprintManager = blueprintManager;
+            this.eventManager = eventManager;
+            this.nameManager = nameManager;
+            this.animations = animations;
+            this.sounds = sounds;
+            this.translator = translator;
+            this.generator = generator;
+            this.achievements = achievements;
+            this.shipFamilies = shipFamilies;
         }
 
         public void freeResources() {

src/main/java/xyz/znix/xftl/game/MainGame.java
@@ -8,7 +8,6 @@ import org.jdom2.output.XMLOutputter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.opengl.PNGDecoder;
 import org.newdawn.slick.util.InputAdapter;
 import xyz.znix.xftl.Datafile;
 import xyz.znix.xftl.VanillaDatafile;
@@ -114,7 +113,7 @@ public class MainGame implements Game {
     }
 
     public void startNewGame(@NotNull String shipName, Difficulty difficulty, EditableShip customised) {
-        InGameState inGameState = new InGameState(this, content, gameContainer, shipName, difficulty, customised);
+        InGameState inGameState = new InGameState(this, content, shipName, difficulty, customised);
         setCurrentState(inGameState);
     }
 
@@ -124,7 +123,7 @@ public class MainGame implements Game {
     }
 
     public void loadSavedGame(Document savedGame) {
-        InGameState inGameState = new InGameState(this, content, gameContainer, savedGame);
+        InGameState inGameState = new InGameState(this, content, savedGame);
         setCurrentState(inGameState);
     }
 
@@ -183,7 +182,7 @@ public class MainGame implements Game {
         Document savedGame;
         try {
             savedGame = oldGame.saveGameState();
-            newGame = new InGameState(this, content, gameContainer, savedGame);
+            newGame = new InGameState(this, content, savedGame);
         } catch (Exception ex) {
             oldGame.debugFailedSaveRestore();
             ex.printStackTrace();

commit c5843d9c9f46a8e26bbedb7f7fa382fdd7b99f15

Fix advanced edition overrides being ignored
This fixes a regression caused by 38ed63bd69d0, which ignores all the
override blueprints (blueprints with a name starting with OVERRIDE_
clobber the non-AE versions with the otherwise same name).

src/main/java/xyz/znix/xftl/Blueprints.kt
@@ -24,6 +24,19 @@ class BlueprintManager {
         // Load all the blueprints
         loader(this)
 
+        // If AE is enabled, rename all the OVERRIDE blueprints to
+        // remove that prefix - thus they'll be used instead of the
+        // original ones.
+        if (enableAE) {
+            // Use toList to duplicate the entries list, since we'll
+            // be mutating the main blueprints map.
+            for ((name, bp) in blueprints.entries.toList()) {
+                if (!name.startsWith(AE_PREFIX))
+                    continue
+                blueprints[name.removePrefix(AE_PREFIX)] = bp
+            }
+        }
+
         // Remove the now-redundant override entries - this is probably
         // unnecessary, but it'll ensure we don't end up accidentally
         // using something from AE in non-AE mode.
@@ -36,19 +49,6 @@ class BlueprintManager {
         for (bp in blueprints.values) {
             if (bp !is BlueprintList) continue
             bp.cleanup()
-        }
-
-        // If AE is enabled, rename all the OVERRIDE blueprints to
-        // remove that prefix - thus they'll be used instead of the
-        // original ones.
-        if (enableAE) {
-            // Use toList to duplicate the entries list, since we'll
-            // be mutating the main blueprints map.
-            for ((name, bp) in blueprints.entries.toList()) {
-                if (!name.startsWith(AE_PREFIX))
-                    continue
-                blueprints[name.removePrefix(AE_PREFIX)] = bp
-            }
         }
     }
 